d002e3b7e7feb24432c1431f45bd15010e35d2da
wangdong <wangdong@youguu.com>
2019-01-21 11:48:56 +0800

    修改

--- src/main/java/com/icome/web/vote/action/VoteAction.java
+++ src/main/java/com/icome/web/vote/action/VoteAction.java
@@ -2,6 +2,7 @@ package com.icome.web.vote.action;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.icome.api.IComeApi;
 import com.icome.pojo.Program;
 import com.icome.pojo.ProgramDetail;
 import com.icome.service.IProgramService;
@@ -32,6 +33,10 @@ public class VoteAction {
 
     @Resource
     private IProgramService programService;
+
+    @Resource
+    private IComeApi comeApi;
+
     /**
      * 根据ticket查询用户信息
      *
@@ -52,14 +57,21 @@ public class VoteAction {
 
         result.put("message","ok");
 
+        JSONObject data = comeApi.getUserDetail(ticket);
+
+        String eId = null;
+        if(data!=null){
+            eId = data.getString("eId");
+        }
+
         int vote = 0 ; //0 已经投票   1 未投票
 
         List<Program> list = programService.list();
 
         result.put("list",list);
 
-        if(ticket!=null){
-            ProgramDetail pd =  programService.getProgramDetail(ticket);
+        if(eId!=null){
+            ProgramDetail pd =  programService.getProgramDetail(eId);
             if(pd==null){
                 vote = 1;
             }else{
@@ -90,7 +102,9 @@ public class VoteAction {
 
         JSONObject result  = new JSONObject();
 
-        if(ticket==null || "".equals(ticket)){
+        JSONObject data = comeApi.getUserDetail(ticket);
+
+        if(data==null || "".equals(data)){
             result.put("status","0001");
 
             result.put("message","无效投票");
@@ -98,10 +112,13 @@ public class VoteAction {
             return result.toJSONString();
         }
 
+        String eId = data.getString("eId");
+
+
         result.put("status","0000");
 
         result.put("message","投票成功");
-        int voteRes = programService.vote(ticket,id);
+        int voteRes = programService.vote(eId,id);
 
         if(voteRes > 0 ){
             result.put("status","0000");
